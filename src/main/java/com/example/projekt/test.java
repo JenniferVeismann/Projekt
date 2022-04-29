3bfd6ef6b9b2fd80d2f4a21eada72976cc3c81be
ketlyiouriev <ketly.iouriev4@gmail.com>
2022-04-29 22:26:13 +0300

    proov!

--- src/main/java/com/example/projekt/test.java
+++ src/main/java/com/example/projekt/test.java
@@ -2,6 +2,6 @@ package com.example.projekt;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("tere");
+        System.out.println("terekest");
     }
 }
